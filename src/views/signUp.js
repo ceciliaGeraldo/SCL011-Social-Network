53e472019ed9a728a5bf3ddb6b7b20171db93eb1
Cecilia Geraldo <ceciliandrea.geraldo@gmail.com>
2019-10-09 18:10:33 -0300

    Registro de usuario mediante correo y contraseña, funcional y testeado en browser

--- src/views/signUp.js
+++ src/views/signUp.js
@@ -1,25 +1,28 @@
 
 //Vista de registros
-
+import { createUser } from '/models/auth.js'
 
 export const templateSignUp = () =>{
 const divContainer = document.getElementById('root');
 divContainer.innerHTML+=
         `<main>
             <div>
-                <input type="text" id="name" placeholder="Ingrese correo">
+                <input type="text" id="name" placeholder="Ingrese Nombre">
                 <input type="email" id="email" placeholder="Ingrese correo">
                 <input type="password" id="password" placeholder="Ingrese contraseña">
                 <button id="register">Registrarse</button>
             </div>
         </main>`
+
+const  send = document.getElementById("register");
+send.addEventListener("click", () => {
+
+let email =document.getElementById("email").value;
+let password =document.getElementById("password").value;
+createUser(email,password);
+})
+        
 }
 
-// let  send = document.getElementById("register")
-// send.addEventListener("click", () => {
 
-// let email =document.getElementById("email").value
-// let password =document.getElementById("pasword").value
-
-// })
 

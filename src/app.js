be9663dc60eaa4219d24c1241bd41057e66f4640
mafda-lennon <mf.lennon@gmail.com>
2019-10-10 16:08:03 -0300

    se cambia nombre login x login2 para trabajar css sobre ese archivo. Agregado logo e icono (png y jpg)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 
 import { initFirebase } from '/assets/js/initFirebase.js';
-import {templateSignUp} from '/assets/views/signUp.js';
+//import {templateSignUp} from '/assets/views/signUp.js';
+import { templateCSS } from './assets/views/logIn2.js';
 
 //Inicializamos Firebase y el observador
 const init = () => {
 
     initFirebase();
-    templateSignUp();
+    templateCSS();
 
 }
 

ff71bd8262889171559fd6234d353df7fc671992
martcamila1 <martcamila1@gmail.com>
2019-10-10 13:52:39 -0400

    arreglo carpetas,correo de verificacion , agregar  eslint

--- src/assets/views/signUp.js
+++ src/assets/views/signUp.js
@@ -1,19 +1,22 @@
 
 //Vista de registros
-import { createUser } from '/models/auth.js'
+import { createUser } from '/assets/js/auth.js'
 
 export const templateSignUp = () =>{
 const divContainer = document.getElementById('root');
 divContainer.innerHTML+=
         `<main>
             <div>
+                <button id="Login">Login</button>
+                <button id="SignIn">Sign in</button>
                 <input type="text" id="name" placeholder="Ingrese Nombre">
                 <input type="email" id="email" placeholder="Ingrese correo">
                 <input type="password" id="password" placeholder="Ingrese contraseña">
                 <button id="register">Registrarse</button>
+                <button id="google">Google</button>
+                <button id="facebook">Facebook</button>
             </div>
         </main>`
-
 
 const  send = document.getElementById("register");
 send.addEventListener("click", () => {
@@ -24,3 +27,4 @@ createUser(email,password);
 })
         
 }
+

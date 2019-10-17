0092b0900aebe8e68b994fb33d95cba05d383f86
mafda-lennon <mf.lennon@gmail.com>
2019-10-17 18:09:04 -0300

    imagen banner home, pequeño cambio en texto botón publicar

--- src/assets/views/feed.js
+++ src/assets/views/feed.js
@@ -8,7 +8,7 @@ export const initFeed = () => {
                 <div>
                     <h1>Hola Mundo, bienvenid@ al feed</h1>
                     <textarea class=""  id="textareaPost" ></textarea>  
-                    <button id="post">Publicar Sesión</button>
+                    <button id="post">Publicar</button>
                     <button id="signOut">Cerrar Sesión</button>
                     <div></div>
                 </div>

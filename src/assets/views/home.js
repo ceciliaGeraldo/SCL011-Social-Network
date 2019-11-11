076f4a67dccff700cd22486892c81dd3f837551c
ceciliaGeraldo <ceciliandrea.geraldo@gmail.com>
2019-11-11 13:14:14 -0300

    modificaciones css

--- src/assets/views/home.js
+++ src/assets/views/home.js
@@ -2,17 +2,17 @@ export const templateHome = () => {
     const divContainer = document.getElementById('root');
     divContainer.innerHTML +=
         `<header>
-            <div id="topNavHome">
                 <a href="#/home"><img src="/assets/images/cultivate-logo.png" alt="cultivate" class="logo"></a>
                 <button class="btn-nav" id="signUp">Registrarse</button>
                 <button class="btn-nav" id="login">Iniciar Sesión</button>
                 
                
-            </div>
+           
         </header>
         <main>
-            <div id="bannerContainer">      
-                <img src="/assets/images/img-banner-home.jpg" alt="bannerSeed" class="banner">
+            <div class="container">      
+                <img src="/assets/images/bannerCultivate2.png" alt="bannerSeed" class="banner">
+                <button class="btn">Únete </button>   
             </div> 
                   
               
